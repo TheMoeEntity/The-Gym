b075174fb3f0934d83a2b3e91ec7e425fbb35c2e
Moses Nwigberi <mosesnwigberi@gmail.com>
2022-11-06 02:52:15 +0100

    Added transitions to hero section

--- Assets/index.js
+++ Assets/index.js
@@ -7,3 +7,26 @@ const closeSideBar = () => {
 	document.querySelector("#sidebarbackground").style.display = 'none'
 	document.querySelector("#sidebar").classList.remove("animateIn")
 }
+
+const flexGuy = document.querySelector("#flexguy")
+const transform = document.querySelector("#transform")
+
+window.onload = ()=> {
+	animateIn(transform,50,"animateIn")
+}
+
+window.onscroll = ()=> {
+	animateIn(flexGuy,150,"animateIn")
+}
+
+const animateIn = (element,revealpoint=150,animation) => {
+	let height = window.innerHeight
+    let revealTop = element.getBoundingClientRect().top
+  
+    if (revealTop < height - revealpoint) { 
+      element.classList.add(animation)
+    } else {
+		element.classList.remove(animation)
+
+    }
+}
